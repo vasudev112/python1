e3aa97c069e1e2fe4168b2abb2c2da5f29a8ebbb
vasudev112 <dadivasudeva112@gmail.com>
2024-11-27 12:17:35 +0530

    hii

--- 26-nov.py
+++ 26-nov.py
@@ -3,8 +3,7 @@ a = "wellcome to python"
 aa = a.count("l")
 print(aa)
 
-# Python Code to check if two Strings are anagram of 
-# each other using sorting 2
+# Python Code to check if two Strings are anagram of each other using sorting 2
 def areAnagrams(s1, s2):
     s1 = sorted(s1)
     s2 = sorted(s2)
@@ -16,14 +15,14 @@ if __name__ == "__main__":
     print("true" if areAnagrams(s1, s2) else "false")
 
 # Write a program to check whether a string is palindrome or not in python 3
-def is_palindrome(s):
+def isPalindrome(s):
     return s == s[::-1]
-
-string = "amaama"
-if is_palindrome(string):
-    print("The entered string is palindrome")
+s = "proogram"
+a = isPalindrome(s)
+if a:
+    print("Yes")
 else:
-    print("The entered string is not palindrome")    
+    print("No")    
 
 #replace the string space with a given character 4
 a = "hello python"
@@ -78,10 +77,12 @@ r = a+s
 print(r)
 
 #remove repeated character from string 12
-def remove_duplicates(input_string):
-    return ''.join(dict.fromkeys(input_string))
-
-print(remove_duplicates("programming"))
+string = "proogram"
+p = " "
+for char in string:
+	if char not in p:
+		p = p+char
+print(p)
 
 # character is vowel or consonant. 13
 l = ['a', 'e', 'i', 'o', 'c'] 
@@ -122,43 +123,21 @@ print("Total Number of Vowels in this String = ", vowels)
 print("Total Number of Consonants in this String = ", consonants)
 
 # print the highest frequency character in string 17
-string= "python"
-print(string)
-char_freq={}
-for i in string:
-    if i in char_freq:
-        char_freq[i]=char_freq[i]+1
-    else:
-        char_freq[i] = 1
-result= max(char_freq, key = char_freq.get)
-print("Most frequent character: ",result)
+a = "GeeksforGeeks"
+s = max(a, key=lambda x: a.count(x))
+print(s)
 
 # replace first occurrence of vowel with "-" in strinh 18
-str = "An apple A day keeps doctor Away."
-str = str.replace('a', '$')
-str = str.replace('a'.upper(),'$')
-print("Modified string : ")
-print(str)
+s = "An apple A day keeps doctor Away."
+s = s.replace('a', '$')
+print(s)
 
 # count alphabets ,digits and special characters 19
- 
-def count_characters(input_string):
-    alphabets = 0
-    digits = 0
-    special_chars = 0
-    for char in input_string:
-        if char.isalpha(): 
-            alphabets += 1
-        elif char.isdigit(): 
-            digits += 1
-        else: 
-            special_chars += 1
-    return alphabets, digits, special_chars
-input_string = input("Enter a string: ")
-alphabets, digits, special_chars = count_characters(input_string)
-print(f"Alphabets: {alphabets}")
-print(f"Digits: {digits}")
-print(f"Special characters: {special_chars}")
+string = "program123!@"
+total_letters = sum([1 for char in string if char.isalpha()])
+total_digits = sum([1 for char in string if char.isdigit()])
+print("Total letters found :-", total_letters)
+print("Total digits found :-", total_digits)
 
 # chicking character is digit or not using isdigit()method 20
 s = "12345"
@@ -168,30 +147,18 @@ s2 = "1234a5"
 print(s2.isdigit())
 print("......")
 # to calculate sum of integers in string 21
-
-def sum_of_integers_in_string(input_string):
-    total_sum = 0
-    temp = ""  
-    for char in input_string:
-        if char.isdigit(): 
-            temp += char 
-        else:
-            if temp: 
-                total_sum += int(temp)
-                temp = "" 
-    if temp: 
-        total_sum += int(temp)
-    return total_sum
-input_string = input("Enter a string: ")
-result = sum_of_integers_in_string(input_string)
-print("Sum of integers in the string:", result)
+def getSum(n): 
+    sum = 0
+    for digit in str(n):  
+      sum += int(digit)       
+    return sum
 
 # print all non repeating character in string 22
 
-String = "prepinsta"
-for i in String:
+S = "prepinsta"
+for i in S:
     count = 0
-    for j in String:
+    for j in S:
         if i == j:
             count+=1
         if count > 1:
@@ -201,12 +168,12 @@ for i in String:
 
 #copy one string to another string 23
 
-def copy_string(source_string):
-    destination_string = source_string
-    return destination_string
-source_string = input("Enter a string: ")
-destination_string = copy_string(source_string)
-print("Copied string:", destination_string)
+s = input("Please Enter Your Own String : ")
+a = s
+r = s[:]
+print("The Final String : a  = ", a)
+print("The Final String : r  = = ", r)
+
 
 # character is vowel or consonant. 24
 l = ['a', 'e', 'i', 'o', 'c'] 
